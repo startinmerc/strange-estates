ad76843e019022a75a6ec6dcb0d39694825aa712
STM <startinmerc@gmail.com>
2019-05-28 13:36:50 +0100

    add POST route for new listings

--- README.md
+++ README.md
@@ -10,3 +10,9 @@
 ###Layout & basic styling
 * Create header & footer partials
 * Add in Bootstrap
+
+###Creating new listings
+* Setup new listing POST route
+* Add in body-parser
+* Setup route to show form
+* Add basic form
--- app.js
+++ app.js
@@ -1,20 +1,37 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var listings = [
+	{name: "One", image: "https://source.unsplash.com/VhpDAKvVA-Q"},
+	{name: "Two", image: "https://source.unsplash.com/uh0u8OH4zuE"},
+	{name: "Three", image: "https://source.unsplash.com/dqZGqFU4Usk"}
+];
 
 app.get("/", function(req,res){
 	res.render("landing");
 });
 
 app.get("/listings", function(req,res){
-	var listings = [
-		{name: "One", image: "https://source.unsplash.com/VhpDAKvVA-Q"},
-		{name: "Two", image: "https://source.unsplash.com/uh0u8OH4zuE"},
-		{name: "Three", image: "https://source.unsplash.com/dqZGqFU4Usk"}
-	];
+
 	res.render("listings", {listings:listings});
 });
+
+app.get("/listings/new", function(req,res){
+	res.render("new");
+});
+
+app.post("/listings", function(req,res){
+	var name = req.body.name;
+	var image = req.body.image;
+	var newListing = {name: name, image: image};
+	listings.push(newListing);
+	res.redirect("/listings");
+});
+
 
 app.listen(3000, process.env.IP, function(){
 	console.log("Server Running")
