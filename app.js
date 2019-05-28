dda6801d80c210ebfb051be842832d6d399075b9
STM <startinmerc@gmail.com>
2019-05-28 12:56:10 +0100

    update routing, add basic listings array

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 var express = require("express");
 var app = express();
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.set("view engine", "ejs");
+
+app.get("/", function(req,res){
+	res.render("landing");
+});
+
+app.get("/listings", function(req,res){
+	var listings = [
+		{name: "One", image: "https://source.unsplash.com/VhpDAKvVA-Q"},
+		{name: "Two", image: "https://source.unsplash.com/uh0u8OH4zuE"},
+		{name: "Three", image: "https://source.unsplash.com/dqZGqFU4Usk"}
+	];
+	res.render("listings", {listings:listings});
+});
+
+app.listen(3000, process.env.IP, function(){
 	console.log("Server Running")
-})
+});
