9cd2edab705a1b3f5c3a17a8fa680187863d60d9
STM <startinmerc@gmail.com>
2019-06-21 13:30:01 +0100

    add rating to comment routes, schema

--- models/comment.js
+++ models/comment.js
@@ -1,14 +1,34 @@
 var mongoose = require("mongoose");
 
 var commentSchema = new mongoose.Schema({
-	text: String,
+	text: {
+		type: String
+	},
 	author: {
 		id: {
 			type: mongoose.Schema.Types.ObjectId,
 			ref: "User"
 		},
-		username: String
+		username: {
+			type: String
+		}
+	},
+	rating: {
+		type: Number,
+		// required: "Please provide a 1-5 rating",
+		min: 1,
+		max: 5,
+		// validate: {
+			// validator: Number.isInteger,
+			// message: "Rating must be a whole number"
+		// }
+	},
+	listing: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Listing"
 	}
+},{
+	timestamps: true
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
--- routes/comments.js
+++ routes/comments.js
@@ -4,11 +4,29 @@ var Listing = require("../models/listing");
 var Comment = require("../models/comment");
 var middleware = require("../middleware");
 
+// Index
+// Shows all comments for a site, to be replaced by dropdown in listing show page
+router.get("/", function(req,res){
+	Listing.findById(req.params.id).populate({
+		path: "comments",
+		options: {
+			sort: {createdAt: -1}
+		}
+	}).exec(function(err, listing) {
+		if(err || !listing){
+			req.flash("error", err.message);
+			return res.redirect("back");
+		}
+		res.render("comments/index", {listing: listing});
+	});
+});
+
+
 // Create new comment
-router.get("/new", middleware.isLoggedIn, function(req,res){
+router.get("/new", middleware.isLoggedIn, middleware.checkCommentExistence, function(req,res){
 	Listing.findById(req.params.id, function(err,listing){
 		if (err) {
-			req.flash("error", "Database error");
+			req.flash("error", err.message);
 			res.redirect("back");
 		} else {
 			res.render("comments/new", {listing:listing});
@@ -17,28 +35,27 @@ router.get("/new", middleware.isLoggedIn, function(req,res){
 });
 
 // Post comment
-router.post("/", middleware.isLoggedIn, function(req,res){
-	Listing.findById(req.params.id, function(err,listing){
+router.post("/", middleware.isLoggedIn, middleware.checkCommentExistence, function(req,res){
+	Listing.findById(req.params.id).populate("comments").exec(function(err,foundListing){
 		if (err) {
-			req.flash("error", "Database error");
-			res.redirect("/listings");
-		} else {
-			Comment.create(req.body.comment, function(err,comment){
-				if (err) {
-					req.flash("error", "Database error");
-					res.redirect("back");
-				} else {
-					// add username & id
-					comment.author.username = req.user.username;
-					comment.author.id = req.user._id;
-					comment.save();
-					listing.comments.push(comment);
-					listing.save();
-					req.flash("success", "Comment posted");
-					res.redirect("/listings/" + listing._id);
-				}
-			});
+			req.flash("error", err.message);
+			return res.redirect("back");
 		}
+		Comment.create(req.body.comment, function(err,comment){
+			if (err) {
+				req.flash("error", err.message);
+				return res.redirect("back");
+			} 
+			comment.author.username = req.user.username;
+			comment.author.id = req.user._id;
+			comment.listing = foundListing;
+			comment.save();
+			foundListing.comments.push(comment);
+			foundListing.rating = middleware.calculateAverage(foundListing.comments);
+			foundListing.save();
+			req.flash("success", "Comment posted");
+			res.redirect("/listings/" + foundListing._id);
+		});
 	});
 });
 
@@ -46,15 +63,15 @@ router.post("/", middleware.isLoggedIn, function(req,res){
 router.get("/:comment_id/edit", middleware.checkCommentOwnership, function(req,res){
 	Listing.findById(req.params.id, function(err, foundListing){
 		if(err || !foundListing){
-			req.flash("error", "Listing not found");
+			req.flash("error", err.message);
 			res.redirect("back");
 		}
 		Comment.findById(req.params.comment_id, function(err, foundComment){
 			if (err || !foundComment) {
-				req.flash("error", "Database error");
+				req.flash("error", err.message);
 				res.redirect("back");
 			} else {
-				res.render("comments/edit", {listing_id: req.params.id, comment: foundComment});
+				res.render("comments/edit", {listing: foundListing, comment: foundComment});
 			}
 		});
 	})
@@ -62,14 +79,21 @@ router.get("/:comment_id/edit", middleware.checkCommentOwnership, function(req,r
 
 // Update comment
 router.put("/:comment_id", middleware.checkCommentOwnership, function(req,res){
-	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, {new: true}, function(err, updatedComment){
 		if (err || !updatedComment) {
-			req.flash("error", "Database error");
-			res.redirect("back");
-		} else {
+			req.flash("error", err.message);
+			return res.redirect("back");
+		}
+		Listing.findById(req.params.id).populate("comments").exec(function(err,foundListing){
+			if (err || !foundListing) {
+				req.flash("error", err.message);
+				return res.redirect("back");
+			}
+			foundListing.rating = middleware.calculateAverage(foundListing.comments);
+			foundListing.save();
 			req.flash("success", "Comment updated");
 			res.redirect("/listings/" + req.params.id);
-		}
+		});
 	});
 });
 
@@ -77,12 +101,21 @@ router.put("/:comment_id", middleware.checkCommentOwnership, function(req,res){
 router.delete("/:comment_id", middleware.checkCommentOwnership, function(req,res){
 	Comment.findByIdAndRemove(req.params.comment_id, function(err){
 		if (err) {
-			req.flash("error", "Database error");
-			res.redirect("back")
-		} else {
-			req.flash("success", "Comment deleted");
-			res.redirect("/listings/" + req.params.id)
+			req.flash("error", err.message);
+			return res.redirect("back")
 		}
+		// $pull removes the deleted comment's reference from the related listing
+		Listing.findByIdAndUpdate(req.params.id, {$pull: {comments: req.params.comment_id}}, {new: true})
+		.populate("comments").exec(function(err,foundListing){
+			if (err || !foundListing) {
+				req.flash("error", err.message);
+				return res.redirect("back");
+			}
+			foundListing.rating = middleware.calculateAverage(foundListing.comments);
+			foundListing.save();
+			req.flash("success", "Comment deleted");
+			res.redirect("/listings/" + req.params.id);
+		});
 	});
 });
 
